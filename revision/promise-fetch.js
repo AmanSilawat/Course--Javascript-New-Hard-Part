b1ab8a5fd5c9a2140c176436b81b790cc8b062f6
aman <aman.khan.silawat.aks@gmail.com>
2020-10-20 23:34:11 +0530

    Added sortest way to use fetch promise object

--- revision/promise-fetch.js
+++ revision/promise-fetch.js
@@ -1,6 +1,7 @@
 // Promise Obj
 let promise = fetch('https://jsonplaceholder.typicode.com/todos/1');
 
+// callback hell
 promise // 
     .then((res) => {
         getData = res.json(); // get JSON data
@@ -9,3 +10,9 @@ promise //
     .then((jsonData) => {
         console.log(jsonData); // print json data
     });
+
+
+// Anoter way to use fetch
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(data => console.log(data));
